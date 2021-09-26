a82cd025df2c2cc2a212d5aee609df74c77dbfdf
Bryan88888 <yingyuyan@hotmail.com>
2021-09-25 21:02:01 -0400

    'init'

--- src/App.js
+++ src/App.js
@@ -1,37 +1,77 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-
-const POKEMONS_PER_PAGE = 20;
+const SpritePrefix = "https://img.pokemondb.net/sprites/home/normal/";
+const POKEMONS_PER_PAGE = 10;
 
 function App() {
   const [data, setData] = useState(null);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  useEffect(() => {
+  const [input, setInput] = useState('');
+  const [err, setErr] = useState(null);
+  
+  const fetchData = useCallback(() => {
+    setLoading(true);
+    setErr(null);
     const offset = (page - 1) * POKEMONS_PER_PAGE;
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${POKEMONS_PER_PAGE}&offset=${offset}`)
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${POKEMONS_PER_PAGE}&offset=${offset}`)
       .then((resp) => resp.json())
       .then((result) => {
         setData(result.results);
         setLoading(false);
-      })
+      });
+  })
+
+  useEffect(() => {
+    fetchData();
   }, [page]);
+
+  const handleSearch = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
+      .then((resp) => resp.json())
+      .then((json) => {
+        setData([json]);
+        setLoading(false);
+        setErr(null);
+      })
+      .catch(() => {
+        setErr(`${input} is not a pokemon`);
+      });
+  };
+
+
+  useEffect(() => {
+    if (!input) {
+      fetchData();
+    }
+  }, [input]);
+
   return (
     <div className="App">
+    <div className='form'>
+        <input
+         type='search'
+         onChange={(e) => setInput(e.target.value)}
+         value={input} 
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
       <div className='container'>
-        {data ? (
-          data.map((v, i) => {
+        {data && !err 
+          ? data.map((v, i) => {
             return (
               <div className='pokemon button'>
                 <p>{v.name}</p>
+                <img src={`${SpritePrefix}${v.name}.png`} alt='' />
               </div>
             );
           })
-        ) : (
-        <p>No Pokemon</p>
-        )}
+         : null}
+
       </div>
+      {err && <p className='error-message'>{err}</p>}
       <div className='pagination'>
         <button
           disabled={page === 1 || loading}
