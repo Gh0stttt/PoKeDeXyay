8bcb6f33041f40f80c6d9e1af1b86505b58b396e
Bryan88888 <yingyuyan@hotmail.com>
2021-10-11 16:39:44 -0400

    'init'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import DetailsPage from "./pages/Details";
 
