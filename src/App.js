1c1530f02d68b2ba9ac93dabcad54443d9d7000c
Bryan88888 <yingyuyan@hotmail.com>
2021-10-11 16:30:09 -0400

    'init'

--- src/App.js
+++ src/App.js
@@ -1,105 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import './App.css';
-import { useCallback, useEffect, useState } from 'react';
+import "./App.css";
 
-const spritePrefix = 'https://img.pokemondb.net/sprites/home/normal/';
-const POKEMONS_PER_PAGE = 10;
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LandingPage from "./pages/Landing";
+import DetailsPage from "./pages/Details";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const [input, setInput] = useState('');
-  const [err, setErr] = useState(null);
-
-  const fetchData = useCallback(() => {
-    setLoading(true);
-    setErr(null);
-    const offset = (page - 1) * POKEMONS_PER_PAGE;
-    fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=${POKEMONS_PER_PAGE}&offset=${offset}`,
-    )
-      .then((resp) => resp.json())
-      .then((result) => {
-        setData(result.results);
-        setLoading(false);
-      });
-  });
-
-  useEffect(() => {
-    fetchData();
-  }, [page]);
-
-  const handleSearch = () => {
-    setLoading(true);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
-      .then((resp) => resp.json())
-      .then((json) => {
-        setData([json]);
-        setLoading(false);
-        setErr(null);
-      })
-      .catch(() => {
-        setErr(`${input} is not a pokemon`);
-        setLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    if (!input) {
-      fetchData();
-    }
-  }, [input]);
-
   return (
-    <div className='App'>
-      <div className='form'>
-        <input
-          type='search'
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleSearch();
-            }
-          }}
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
-      <div className='container'>
-        {data && !err && !loading
-          ? data.map((v, i) => {
-              return (
-                <div className='pokemon button'>
-                  <p>{v.name}</p>
-                  <img src={`${spritePrefix}${v.name}.png`} alt='' />
-                </div>
-              );
-            })
-          : null}
-      </div>
-      {err && !loading && (
-        <div className='error-container'>
-          <p className='error-message'>{err}</p>
-        </div>
-      )}
-      <div className='pagination'>
-        <button
-          disabled={page === 1 || loading}
-          className='buttonNext'
-          onClick={() => setPage(page === 1 ? 1 : page - 1)}
-        >
-          Prev
-        </button>
-        <button
-          disabled={loading}
-          className='buttonNext'
-          onClick={() => setPage(page + 1)}
-        >
-          Next
-        </button>
-      </div>
-      <p className='page-number'>Page {page}</p>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage}></Route>
+          <Route path="/pokemon/:name" component={DetailsPage}>
+           
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Tabs({ tabs, activeTabIndex, setActiveTabIndex }) {
+    return (
+      <div className="tabs-nav">
+        {tabs.map((v, i) => (
+          <div
+            onClick={() => setActiveTabIndex(i)}
+            className={`tab-link${activeTabIndex === i ? "active" : ""}`}
+          >
+            <span>{v}</span>
+            {activeTabIndex === i && <div className="dot" />}
+          </div>
+        ))}
+        {activeTabIndex === 0 && <p> Overview stuff </p>}
+        {activeTabIndex === 1 && <p> Stats stuff </p>}
+        {activeTabIndex === 2 && <p> Abilities stuff </p>}
+      </div>
+    );
+}
+
+export default Tabs
